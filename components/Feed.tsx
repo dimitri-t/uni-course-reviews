5f969c95c669703ca389b2f9fea8eeeff85cbfd8
MartinLe1 <martinle.au@gmail.com>
2021-12-29 22:34:12 +1100

    adding sample review card design

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import { useState } from "react";
 import Review from "./Review";
 
@@ -10,33 +10,46 @@ const Feed = () => {
     {
       'date': '10-12-2021',
       'author': 'John',
-      'review': 'Very good course, highly recommended'
+      'dateCompleted': 'Term 3, 2021',
+      'lecturer': 'Bob Smith',
+      'difficulty': 3,
+      'rating': 3,
+      'review': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac fringilla libero. Nullam semper ultricies eleifend. Vestibulum a sodales velit. Etiam dictum metus ac odio posuere pulvinar eget ac sem. Nunc felis eros, scelerisque nec mauris sed, facilisis porttitor nisi. Donec id bibendum dolor, quis molestie purus. Proin quis facilisis lorem. Morbi ac felis at nunc volutpat maximus. Praesent sed cursus arcu, vel tincidunt metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed facilisis orci mauris, at cursus nisl sollicitudin quis. Proin et augue ut magna luctus venenatis in eget elit. Quisque mauris purus.'
     },
     {
       'date': '23-12-2021',
       'author': 'Maria',
+      'dateCompleted': 'Term 3, 2021',
+      'lecturer': 'Stacy Smith',
+      'difficulty': 5,
+      'rating': 5,
       'review': 'Bad course, dont do it'
     },
     {
       'date': '21-12-2021',
       'author': 'Dim',
+      'dateCompleted': 'Term 3, 2021',
+      'lecturer': 'John Smith',
+      'difficulty': 1,
+      'rating': 5,
       'review': 'Recommended doing this course as an elective'
     }
   ];
 
   return (
-    <Box bg="red">
+    <Box>
+      <Stack spacing={8}>
       {reviews.map((r, i) => {
         return (
           <div key={i}>
             <Review {...r} />
 
           </div>
-        )
-        })
+        )})
       }    
-
+      </Stack>
     </Box>
+
   );
 
 }
--- components/Review.tsx
+++ components/Review.tsx
@@ -1,13 +1,49 @@
-import { Box } from "@chakra-ui/react";
+import { StarIcon, ChevronUpIcon, ChevronDownIcon, ChatIcon } from "@chakra-ui/icons";
+import { Box, Text, Divider, SimpleGrid, Button, ButtonGroup, IconButton, Center, Flex, Spacer } from "@chakra-ui/react";
 import { ReactElement } from "react";
 
-type reviewType = { date: string; author: string; review: string; }
+type reviewType = { date: string; author: string; dateCompleted: string; lecturer: string; difficulty: number; rating: number; review: string; }
 
-const Review = ({date, author, review}) => {
+const Review = ({date, author, dateCompleted, lecturer, difficulty, rating, review}) => {
   return (
-    <Box>
-      {date} {author} {review}
+    <Box bg='white' borderWidth='1px' borderRadius='lg' shadow='md'>
+      <Box padding="3">
+        <Box> <Text fontSize='2xl' fontWeight='bold'>{author}</Text> </Box>
+        <Box> <Text fontSize='lg' fontWeight='bold'>Review Created: {date}</Text> </Box>
+      </Box>
+      <Divider orientation='horizontal'></Divider>
+
+      <Box padding="3" alignContent={"center"}>
+        <SimpleGrid columns={[2, null]} spacing={3}>
+          <Box><Text fontWeight="bold">Term/Year Completed:</Text><Text>{dateCompleted}</Text></Box>
+          <Box>
+            <Text fontWeight="bold">Difficulty:</Text>
+            {Array(5).fill('').map((_,i) => (<StarIcon key={i} color={i <= difficulty ? 'yellow.400': 'gray.300'}></StarIcon>))}
+          </Box>
+          <Box><Text fontWeight='bold'>Lecturer:</Text><Text>{lecturer}</Text></Box>
+          <Box>
+              <Text fontWeight="bold">Overall Rating:</Text>
+              {Array(5).fill('').map((_,i) => (<StarIcon key={i} color={i <= rating ? 'yellow.400': 'gray.300'}></StarIcon>))}
+          </Box>
+        </SimpleGrid>
+      </Box>
+      <Divider orientation='horizontal'></Divider>
+
+      <Box padding='3'>
+        <Box ><Text>{review}</Text></Box>
+      </Box>
+      
+      <Flex padding={3}>
+        <Button leftIcon={<ChatIcon/>}>Comments</Button>
+        <Spacer />
+        <ButtonGroup isAttached>
+          <IconButton aria-label='Upvote' icon={<ChevronUpIcon/>} variant='outline' colorScheme='green'></IconButton>
+          <Center paddingLeft={3} paddingRight={3}><Text fontSize='md'>56</Text></Center>
+          <IconButton aria-label='Downvote' icon={<ChevronDownIcon/>} variant='outline' colorScheme='red'></IconButton>
+        </ButtonGroup>
+      </Flex>
     </Box>
+
   )
 }
 
