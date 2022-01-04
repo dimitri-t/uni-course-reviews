207ce3e0957ba9241ed3628e52b9f73f9b5ff187
Dim <dimdakas@gmail.com>
2022-01-04 15:04:52 +1100

    Added server side rendering of database and a button to submit dummy data for a new review

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,49 +1,16 @@
 import { Box, Stack } from "@chakra-ui/react";
-import { useState } from "react";
-import Review from "./Review";
+import ReviewCard from "./ReviewCard";
+import { Review } from '@prisma/client';
 
-// type reviewType = { date: string; author: string; review: string; }[]
-
-const Feed = () => {
-  // const [reviews, setReviews] = useState(null);
-  const reviews = [
-    {
-      'date': '10-12-2021',
-      'author': 'John',
-      'dateCompleted': 'Term 3, 2021',
-      'lecturer': 'Bob Smith',
-      'difficulty': 3,
-      'rating': 3,
-      'review': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac fringilla libero. Nullam semper ultricies eleifend. Vestibulum a sodales velit. Etiam dictum metus ac odio posuere pulvinar eget ac sem. Nunc felis eros, scelerisque nec mauris sed, facilisis porttitor nisi. Donec id bibendum dolor, quis molestie purus. Proin quis facilisis lorem. Morbi ac felis at nunc volutpat maximus. Praesent sed cursus arcu, vel tincidunt metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed facilisis orci mauris, at cursus nisl sollicitudin quis. Proin et augue ut magna luctus venenatis in eget elit. Quisque mauris purus.'
-    },
-    {
-      'date': '23-12-2021',
-      'author': 'Maria',
-      'dateCompleted': 'Term 3, 2021',
-      'lecturer': 'Stacy Smith',
-      'difficulty': 5,
-      'rating': 1,
-      'review': 'Bad course, dont do it'
-    },
-    {
-      'date': '21-12-2021',
-      'author': 'Dim',
-      'dateCompleted': 'Term 3, 2021',
-      'lecturer': 'John Smith',
-      'difficulty': 1,
-      'rating': 5,
-      'review': 'Recommended doing this course as an elective'
-    }
-  ];
+const Feed = ({ reviews }) => {
 
   return (
     <Box>
       <Stack spacing={8}>
-      {reviews.map((r, i) => {
+      {reviews.map((r: Review, i) => {
         return (
           <div key={i}>
-            <Review {...r} />
-
+            <ReviewCard {...r} />
           </div>
         )})
       }    
@@ -51,7 +18,37 @@ const Feed = () => {
     </Box>
 
   );
-
 }
 
 export default Feed;
+
+
+// initialReviews: [
+//   {
+//     'date': '10-12-2021',
+//     'author': 'John',
+//     'dateCompleted': 'Term 3, 2021',
+//     'lecturer': 'Bob Smith',
+//     'difficulty': 3,
+//     'rating': 3,
+//     'review': 'Praesent sed cursus arcu, vel tincidunt metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed facilisis orci mauris, at cursus nisl sollicitudin quis. Proin et augue ut magna luctus venenatis in eget elit. Quisque mauris purus.'
+//   },
+//   {
+//     'date': '23-12-2021',
+//     'author': 'Maria',
+//     'dateCompleted': 'Term 3, 2021',
+//     'lecturer': 'Stacy Smith',
+//     'difficulty': 5,
+//     'rating': 1,
+//     'review': 'Bad course, dont do it'
+//   },
+//   {
+//     'date': '21-12-2021',
+//     'author': 'Dim',
+//     'dateCompleted': 'Term 3, 2021',
+//     'lecturer': 'John Smith',
+//     'difficulty': 1,
+//     'rating': 5,
+//     'review': 'Recommended doing this course as an elective'
+//   }
+// ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,62 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
+import { Container, Box, Heading, Button } from '@chakra-ui/react';
 import Feed from '../components/Feed';
 
-const Page = () => {
+import { useState } from 'react';
+import { Prisma, PrismaClient, Review } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function getServerSideProps () {
+  // Load first instance of DB
+  const reviews = await prisma.review.findMany();
+
+  return {
+    props: {
+      initialReviews: reviews
+    }
+  }
+}
+
+const saveReview = async (review: Prisma.ReviewCreateInput) => {
+  const response = await fetch('/api/createreview', {
+    method: 'POST',
+    body: JSON.stringify(review)
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return await response.json();
+}
+
+// TODO: Add types
+
+const Page = ({ initialReviews }) => {
+  const [reviews, setReviews] = useState<Review[]>(initialReviews);
   return (
     <Container>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Feed />
+      <Box display={ { md: 'flex' } }>
+        
+        <Box mt="5" flexGrow={ 1 }>
+          <Button colorScheme='blue'
+            onClick={ async () => {
+              try {
+                // TODO: REMOVE THIS DUMMY DATA
+                const newReview = {
+                  author: 'new',
+                  reviewText: 'Very good course',
+                  coursesId: 1
+                };
+                await saveReview(newReview);
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+          >
+            Add Review
+          </Button>
+          <Feed reviews={reviews} />
         </Box>
       </Box>
     </Container>
