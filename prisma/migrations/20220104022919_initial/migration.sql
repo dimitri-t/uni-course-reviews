cd00eccc88fe46514f948c0316998f8fcbd27aa0
Dim <dimdakas@gmail.com>
2022-01-04 15:05:18 +1100

    Changed the db model

--- prisma/migrations/20220104022919_initial/migration.sql
+++ prisma/migrations/20220104022919_initial/migration.sql
@@ -1,14 +1,15 @@
 -- CreateTable
 CREATE TABLE "Courses" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "courseName" TEXT NOT NULL
+    "courseName" TEXT NOT NULL,
+    "courseCode" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Review" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "author" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
+    "reviewText" TEXT NOT NULL,
     "coursesId" INTEGER,
     CONSTRAINT "Review_coursesId_fkey" FOREIGN KEY ("coursesId") REFERENCES "Courses" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
